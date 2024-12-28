6f7a2d8674fc4ac82d61b48a57264de19ce48b71
Hikabu <bledniyadozub@gmail.com>
2024-12-28 00:44:23 -0500

    not worky

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -2,24 +2,24 @@
 
 echo "[WP config] Configuring WordPress..."
 
-DB_HOST=$(cat /project/secrets/db_creds | grep DB_HOST | cut -d '=' -f2 | tr -d '\n'
-WP_DB_NAME=$(cat /project/secrets/db_creds | grep DB_NAME | cut -d '=' -f2 | tr -d '\n')
-WP_DB_USER=$(cat /project/secrets/db_creds | grep DB_USER | cut -d '=' -f2 | tr -d '\n')
-WP_DB_PASS=$(cat /project/secrets/db_creds | grep DB_PASS | cut -d '=' -f2 | tr -d '\n'
+DB_HOST=$(cat /project/secrets/db_creds.txt | grep DB_HOST | cut -d '=' -f2 | tr -d '\n')
+WP_DB_NAME=$(cat /project/secrets/db_creds.txt | grep DB_NAME | cut -d '=' -f2 | tr -d '\n')
+WP_DB_USER=$(cat /project/secrets/db_creds.txt | grep DB_USER | cut -d '=' -f2 | tr -d '\n')
+WP_DB_PASS=$(cat /project/secrets/db_creds.txt | grep DB_PASS | cut -d '=' -f2 | tr -d '\n')
 
 WP_PATH=/var/www
 
-if [ -f ${WP_PATH}/wp-config.php ]
-then
+if [ -f ${WP_PATH}/wp-config.php ]; then
 	echo "[WP config] WordPress already configured."
 else
 	echo "[WP config] Setting up WordPress..."
 	echo "[WP config] Updating WP-CLI..."
 	wp-cli.phar cli update --yes --allow-root
+
 	echo "[WP config] Downloading WordPress..."
 	wp-cli.phar core download --allow-root
-	echo "[WP config] Creating wp-config.php..."
 
+	echo "[WP config] Creating wp-config.php..."
 	cat << EOF > /var/www/wp-config.php
 	<?php
 	define( 'DB_NAME', '${DB_NAME}' );
@@ -32,18 +32,13 @@ else
 	\$table_prefix = 'wp_';
 	define( 'WP_DEBUG', false );
 	if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );}
+		define( 'ABSPATH', __DIR__ . '/' );}
 	require_once ABSPATH . 'wp-settings.php';
 	EOF
 
 	echo "[WP config] Installing WordPress core..."
-
-	echo "[WP config] Creating WordPress default user..."
 fi
-
-
 echo "[WP config] Waiting for MariaDB..."
-
 echo "[WP config] MariaDB accessible."
 
 NGINX_HOST=$(cat /project/secrets/wp_creds | grep URL | cut -d '=' -f2 | tr -d '\n')
@@ -67,7 +62,7 @@ USER1_MAIL=$(cat /project/secrets/wp_creds | grep USER1_MAIL | cut -d '=' -f2 | 
 wp user create  --allow-root --path=/var/www \
                 $USER1_LOGIN \
                 $USER1_MAIL \
-                --user_pass=$USER1_MAIL \
+                --user_pass=$USER1_PASS \
                 --role=$USER1_ROLE
 
 wp plugin update --allow-root --path=/var/www --all
@@ -75,4 +70,4 @@ wp plugin update --allow-root --path=/var/www --all
 
 echo "[WP config] Starting WordPress fastCGI on port 9000."
 exec env -i /usr/sbin/php-fpm83 -F -R
-$@
+fi
